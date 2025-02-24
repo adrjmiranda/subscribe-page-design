4a3a595e1d1342f056a0398bfa8e4162ef20ce5c
Adriano Miranda <adrjmiranda@gmail.com>
2025-02-23 21:30:24 -0300

    style: created style for smaller screens

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -15,7 +15,7 @@
   \**********************/
 /***/ (() => {
 
-eval("console.log(\"It's working!\");\n\n\n//# sourceURL=webpack://subscribe-page-design/./src/index.ts?");
+eval("/**\n * Toggle Navbar Menu\n */\nvar toggleMenuButton = document.getElementById('toggle-menu');\nvar navbarMenu = document.querySelector('.navbar-menu');\nvar navbarMenuLinks = document.querySelectorAll('.navbar-link');\nvar changeIcon = function () {\n    var iconList = document.querySelectorAll('#toggle-menu i');\n    iconList.length &&\n        iconList.forEach(function (icon) { return icon.classList.toggle('hidden'); });\n};\nif (toggleMenuButton && navbarMenu && navbarMenuLinks.length) {\n    toggleMenuButton.addEventListener('click', function () {\n        navbarMenu.classList.toggle('show');\n        changeIcon();\n    });\n    navbarMenuLinks.forEach(function (link) {\n        return link.addEventListener('click', function () {\n            navbarMenu.classList.toggle('show');\n            changeIcon();\n        });\n    });\n}\n\n\n//# sourceURL=webpack://subscribe-page-design/./src/index.ts?");
 
 /***/ })
 
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,34 @@
-console.log("It's working!");
+/**
+ * Toggle Navbar Menu
+ */
+
+const toggleMenuButton = document.getElementById(
+	'toggle-menu'
+) as HTMLButtonElement;
+const navbarMenu = document.querySelector('.navbar-menu') as HTMLUListElement;
+const navbarMenuLinks = document.querySelectorAll(
+	'.navbar-link'
+) as NodeListOf<HTMLAnchorElement>;
+
+const changeIcon = (): void => {
+	const iconList = document.querySelectorAll(
+		'#toggle-menu i'
+	) as NodeListOf<HTMLElement>;
+
+	iconList.length &&
+		iconList.forEach((icon) => icon.classList.toggle('hidden'));
+};
+
+if (toggleMenuButton && navbarMenu && navbarMenuLinks.length) {
+	toggleMenuButton.addEventListener('click', () => {
+		navbarMenu.classList.toggle('show');
+		changeIcon();
+	});
+
+	navbarMenuLinks.forEach((link) =>
+		link.addEventListener('click', () => {
+			navbarMenu.classList.toggle('show');
+			changeIcon();
+		})
+	);
+}
